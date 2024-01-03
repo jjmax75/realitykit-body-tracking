7cc2875dd407a74d72ff73c858c81457d5a06f5e
John Behan <behanj@gmail.com>
2024-01-03 23:04:01 +0000

    Add colouring for joint positions  that are calculated based on tracked joints

--- bodytracking/ContentView.swift
+++ bodytracking/ContentView.swift
@@ -26,7 +26,28 @@ class BodySkeleton: Entity {
             var jointRadius: Float = 0.03
             var jointColor: UIColor = .green
             
-            
+            // Green Joints are actively tracked, Tellow joints follow the closest green parent
+            switch jointName {
+            case "neck_1_joint", "neck_2_joint", "neck_3_joint", "neck_4_joint", "head_joint", "left_shoulder_1_joint", "right_shoulder_1_joint":
+                jointRadius *= 0.5
+            case "jaw_joint", "chin_joint", "left_eye_joint", "left_eyeLowerLid_joint", "left_eyeUpperLid_joint", "left_eyeball_joint", "nose_joint", "right_eye_joint", "right_eyeLowerLid_joint", "right_eyeUpperLid_joint", "right_eyeball_joint":
+                jointRadius *= 0.2
+                jointColor = .yellow
+            case _ where jointName.hasPrefix("spine_"):
+                jointRadius *= 0.75
+            case "left_hand_joint", "right_hand_joint":
+                jointRadius *= 1
+                jointColor = .green
+            case _ where jointName.hasPrefix("left_hand") || jointName.hasPrefix("right_hand"):
+                jointRadius *= 0.25
+                jointColor = .yellow
+            case _ where jointName.hasPrefix("left_toes") || jointName.hasPrefix("right_toes"):
+                jointRadius *= 0.5
+                jointColor = .yellow
+            default:
+                jointRadius = 0.05
+                jointColor = .green
+            }
             
             let jointEntity = makeJoint(radius: jointRadius, color: jointColor)
             joints[jointName] = jointEntity
